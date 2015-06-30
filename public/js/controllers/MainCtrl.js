361d3adfa0fce3f105ad2a142585f155b4a9b3c5
Shankar Morwal <morwal89@gmail.com>
2015-06-30 11:28:03 +0530

    inital

---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,5 @@
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
+
+	$scope.tagline = 'Without requirements or design, programming is the art of adding bugs to an empty text file.';
+
+});
